c2ac3da7e9542d4e26bc9348caacecc38628d2f9
Raveena Vijaya <raveenavijaya@gmail.com>
2020-06-12 11:50:06 -0500

    Updates

---
+++ precache-manifest.73fa03743f14df7fb6c7a587404eabb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4857fb4a8e73195d455ebc0b9cb59a",
+    "url": "/ReactApp_VideoSearch/index.html"
+  },
+  {
+    "revision": "ae2544bff39d77c0dfad",
+    "url": "/ReactApp_VideoSearch/static/css/main.51df330a.chunk.css"
+  },
+  {
+    "revision": "8a8606172ce1aa419361",
+    "url": "/ReactApp_VideoSearch/static/js/2.b3e1cf71.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/ReactApp_VideoSearch/static/js/2.b3e1cf71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2544bff39d77c0dfad",
+    "url": "/ReactApp_VideoSearch/static/js/main.6eeda958.chunk.js"
+  },
+  {
+    "revision": "4a5c2ff66b436924af50",
+    "url": "/ReactApp_VideoSearch/static/js/runtime-main.1707388d.js"
+  }
+]);
